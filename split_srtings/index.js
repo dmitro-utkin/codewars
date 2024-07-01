7ac178ab75ef9ccb4c7a098b33e0f2a260730174
ДМИТРО <sssddd1366628@gmail.com>
2024-07-01 15:05:12 +0300

    added v2

--- split_srtings/index.js
+++ split_srtings/index.js
@@ -19,19 +19,29 @@
 
 const str = "abc";
 
+// v1
+// const solution = (str) => {
+//   let rez = [];
+//   let newStr = str.split('');
+//   for (let i = 0; i < newStr.length; i += 2) {
+//     if (newStr[i + 1] === undefined) {
+//       newStr[i + 1] = '_';
+//     }
+//   };
+
+//   for (let i = 0; i < newStr.length; i += 2) {
+//     rez.push(newStr.slice(i, i + 2).join(''));
+//   }
+//   // console.log(str);
+//   return rez;
+// };
+
+// v2
 const solution = (str) => {
   let rez = [];
-  let newStr = str.split('');
-  for (let i = 0; i < newStr.length; i += 2) {
-    if (newStr[i + 1] === undefined) {
-      newStr[i + 1] = '_';
-    }
-  };
-
-  for (let i = 0; i < newStr.length; i += 2) {
-    rez.push(newStr.slice(i, i + 2).join(''));
+  for (let i = 0; i < str.length; i += 2) {
+    rez.push(str.slice(i, i + 2).padEnd(2, '_'));
   }
-  // console.log(str);
   return rez;
 };
 
